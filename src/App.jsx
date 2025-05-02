9e7731a67541fdb92d1809739be21c643053113c
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2025-05-02 20:07:29 +0600

    fixed ui for 'external svg'

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="h-6 w-6 shrink-0 stroke-current">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
   </svg>
-  <span className='flex gap-1'>
-  Use <a className='flex gap-1 underline' href="https://rafiz001.github.io/sheet-routine/">Sheet Routine <img src='/cover/static/link-external-small-ltr-progressive.svg'/></a> to get teachers name, course code automatic.</span>
+  <span className=''>
+  Use <a className=" underline after:content-[url('/cover/static/link-external-small-ltr-progressive.svg')] after:ml-0.5" href="https://rafiz001.github.io/sheet-routine/">Sheet Routine</a> to get teachers name, course code automatic.</span>
 </div>
 
 
