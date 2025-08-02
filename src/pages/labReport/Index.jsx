7d52c6a8fbe3db43c05ddb00e988a156399ea05e
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2025-08-02 12:16:09 +0600

    fixed hasTitle feld to work the label

--- src/pages/labReport/Index.jsx
+++ src/pages/labReport/Index.jsx
@@ -182,9 +182,9 @@ function Index(props) {
           </label>
         </div>
         <div>
-          <label htmlFor='willIssue'>Has Title: </label>
+          <label htmlFor='hasTitle'>Has Title: </label>
           <label className="toggle text-base-content">
-            <input id='willIssue' checked={data.hasTitle} type="checkbox" onChange={() => { setData(({ ...data, hasTitle: !(data.hasTitle) })); }} />
+            <input id='hasTitle' checked={data.hasTitle} type="checkbox" onChange={() => { setData(({ ...data, hasTitle: !(data.hasTitle) })); }} />
 
             <svg aria-label="disabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
             <svg aria-label="enabled" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="4" fill="none" stroke="currentColor"><path d="M20 6 9 17l-5-5"></path></g></svg>
